3a54db6ae42caaea62ebd14566b100b436be7dac
Thomas Mailund <mailund@birc.au.dk>
2022-03-24 08:24:03 +0100

    bucket sort

--- src/radix.c
+++ src/radix.c
@@ -6,7 +6,8 @@
 typedef int letter_count[256];
 static inline void count_letters(letter_count *c, size_t n, const char x[n])
 {
-    for (size_t i = 0; i < n - 1; i++)
+    memset(c, 0, sizeof *c);
+    for (size_t i = 0; i < n; i++)
     {
         (*c)[(unsigned)x[i]]++;
     }
@@ -14,8 +15,8 @@ static inline void count_letters(letter_count *c, size_t n, const char x[n])
 
 void count_sort(size_t n, const char x[n], char y[n])
 {
-    letter_count count = {0};
-    count_letters(&count, n, x);
+    letter_count count;
+    count_letters(&count, n - 1, x); // don't include sentinel here
     for (int i = 0; i < 256; i++)
     {
         for (int k = 0; k < count[i]; k++)
@@ -26,8 +27,27 @@ void count_sort(size_t n, const char x[n], char y[n])
     *y = '\0';
 }
 
+typedef int buckets[256];
+static inline void cumsum(buckets *b, letter_count *c)
+{
+    int sum = 0;
+    for (int i = 0; i < 256; i++)
+    {
+        (*b)[i] = sum;
+        sum += (*c)[i];
+    }
+}
+
 void bucket_sort(size_t n, const char x[n], const int idx[n], int out[n])
 {
+    letter_count c;
+    count_letters(&c, n, x);
+    buckets b;
+    cumsum(&b, &c);
+    for (size_t i = 0; i < n; i++)
+    {
+        out[b[(unsigned)x[idx[i]]]++] = idx[i];
+    }
 }
 
 void lsd_radix_sort(size_t n, const char x[n], int sa[n])
--- src/radix_test.c
+++ src/radix_test.c
@@ -18,8 +18,36 @@ static void check_count_sort(void)
     check_count_sort_output("", "");
 }
 
+static void check_bucket_sort_output(const char *x, int idx[], int expected[])
+{
+    size_t n = strlen(x) + 1;
+    int out[n];
+    bucket_sort(n, x, idx, out);
+    for (size_t i = 0; i < n; i++)
+    {
+        assert(out[i] == expected[i]);
+    }
+}
+
+static void check_bucket_sort(void)
+{
+    check_bucket_sort_output(
+        "abaab", (int[]){0, 1, 2, 3, 4, 5}, (int[]){5, 0, 2, 3, 1, 4});
+    check_bucket_sort_output(
+        "abaab", (int[]){5, 4, 3, 2, 1, 0}, (int[]){5, 3, 2, 0, 4, 1});
+    check_bucket_sort_output(
+        "mississippi",
+        (int[]){0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11},
+        (int[]){11, 1, 4, 7, 10, 0, 8, 9, 2, 3, 5, 6});
+    check_bucket_sort_output(
+        "mississippi",
+        (int[]){11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+        (int[]){11, 10, 7, 4, 1, 0, 9, 8, 6, 5, 3, 2});
+}
+
 int main(void)
 {
     check_count_sort();
+    check_bucket_sort();
     return 0;
 }
